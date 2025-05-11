496fbec8b2e693a7909caf221c4551ad974c969c
Ashish <ashishcoorg46@gmail.com>
2025-05-12 01:15:18 +0530

    added port env option

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Proper dynamic import for fetch
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
